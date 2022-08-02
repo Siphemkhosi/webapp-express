40ec1457367d9b5510f324b60965034e5e2535c8
SipheMkhosi <siphemkhosi7@gmail.com>
2022-08-02 16:00:09 +0200

    fixed path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 
-const SettingsBill = require("./js/settings-Bill");
+const SettingsBill = require("./settings-Bill");
 const app = express();
 const settingsBill = SettingsBill();
 
